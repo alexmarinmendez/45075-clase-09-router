0a38137b0ed8481aa67c53a906defae9987dfaac
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-02-06 19:36:46 -0500

    feat: add a custom middleware

--- src/app.js
+++ src/app.js
@@ -15,7 +15,16 @@ app.listen(8080, () => console.log('Server Up'))
 
 app.use(express.json())
 app.use(express.urlencoded({extends: true}))
+
+function ejecutaAlgo(req, res, next) {
+    req.dato1 = "coder"
+    console.log(req.dato1)
+    if (true) res.send('error')
+    else next()
+}
+
 // app.use(express.static('public'))
 app.use('/contenido', express.static('public'))
+app.use(ejecutaAlgo)
 app.use('/users', usersRouter)
 app.use('/pets', petsRouter)
